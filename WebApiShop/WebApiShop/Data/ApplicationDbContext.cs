55829e423210970cbb106206b964f81c35960338
daaammirka <cdbacjdorz@yandex.com>
2023-10-16 22:23:03 +0300

    Прописаны отношения между объектами (many-to-many)

--- WebApiShop/WebApiShop/Data/ApplicationDbContext.cs
+++ WebApiShop/WebApiShop/Data/ApplicationDbContext.cs
@@ -10,4 +10,22 @@ public class ApplicationDbContext : DbContext
     
     public DbSet<Employee?> Employees { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<EmployeeProduct> EmployeeProducts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<EmployeeProduct>().HasKey(x => new { x.EmployeeId, x.ProductId });
+
+        modelBuilder.Entity<EmployeeProduct>()
+            .HasOne(x => x.Employees)
+            .WithMany(employee => employee.ProductsList)
+            .HasForeignKey(x => x.EmployeeId);
+
+        modelBuilder.Entity<EmployeeProduct>()
+            .HasOne(x => x.Products)
+            .WithMany(product => product.EmployeesList)
+            .HasForeignKey(x => x.ProductId);
+    }
 }
