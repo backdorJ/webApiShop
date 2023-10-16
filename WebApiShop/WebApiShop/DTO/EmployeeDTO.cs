dc159b54aedd3521db36149ed7d4b5161f5cdaf6
daaammirka <cdbacjdorz@yandex.com>
2023-10-16 22:21:00 +0300

    Добавлены DTO объекты для отображения при запросе

---
+++ WebApiShop/WebApiShop/DTO/EmployeeDTO.cs
@@ -0,0 +1,9 @@
+namespace WebApiShop.DTO;
+
+public class EmployeeDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
